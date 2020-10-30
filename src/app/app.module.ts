7ac8ca43c2f96e72122e18e1d6adbf9217277a48
Aman Verma <amannverma63@gmail.com>
2020-10-30 12:04:28 +0530

    added routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
-import { DetailsComponent } from './details/details.component';
 import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
+import { Routes, RouterModule } from '@angular/router';
+
+import { DetailsComponent } from './details/details.component';
+import { ErrorComponent } from './error/error.component';
 import { RequestService } from './request.service';
 
+const appRoutes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: DetailsComponent },
+  { path: 'not-found', component: ErrorComponent },
+  { path: '**', redirectTo: 'not-found' },
+];
 @NgModule({
-  declarations: [AppComponent, DetailsComponent],
-  imports: [BrowserModule, HttpClientModule],
+  declarations: [AppComponent, DetailsComponent, ErrorComponent],
+  imports: [BrowserModule, HttpClientModule, RouterModule.forRoot(appRoutes)],
   providers: [RequestService],
   bootstrap: [AppComponent],
 })
--- src/app/request.service.ts
+++ src/app/request.service.ts
@@ -3,11 +3,9 @@ import { Injectable } from '@angular/core';
 
 @Injectable({ providedIn: 'root' })
 export class RequestService {
-  data;
-
   constructor(private http: HttpClient) {}
 
-  sendRequest(path: string) {
+  sendRequest(path: string): any {
     return this.http.get(path);
   }
 }
